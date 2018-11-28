1d22c03f97e55077d9fed60c2263756803b8fddc
AndreSalazar96 <asalazar@analiticom.com>
2018-11-28 13:19:15 -0400

    create-404-page

---
+++ includes/no-results.php
@@ -0,0 +1,6 @@
+<div class="entry">
+<!--If no results are found-->
+	<h1><?php esc_html_e('404','Divi'); ?></h1>
+	<p><?php esc_html_e('Oh, wow! Do not worry, come back with us ','Divi'); ?> <a href="analiticomsite.flywheelsites.com"><strong class='strong-blue-404'>here.</strong></a></p>
+</div>
+<!--End if no results are found-->
