bb555da287dba984d33a95de2987fccd604f0eeb
AndreSalazar96 <asalazar@analiticom.com>
2018-11-30 14:34:26 -0400

    'change-url-analiticom'

--- includes/no-results.php
+++ includes/no-results.php
@@ -1,6 +1,6 @@
 <div class="entry">
 <!--If no results are found-->
 	<h1><?php esc_html_e('404','Divi'); ?></h1>
-	<p><?php esc_html_e('Oh, wow! Do not worry, ','Divi'); ?> <span class='text-responsive-white'>come back with us</span><a href="http://analiticomsite.flywheelsites.com/" class='redirect-btn-orange'><strong class='strong-blue-404'>here.</strong></a></p>
+	<p><?php esc_html_e('Oh, wow! Do not worry, ','Divi'); ?> <span class='text-responsive-white'>come back with us</span><a href="http://analiticom.com/" class='redirect-btn-orange'><strong class='strong-blue-404'>here.</strong></a></p>
 </div>
 <!--End if no results are found-->
